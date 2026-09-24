bbd4b22481821d3f685207aa9ce7aaedd73f12af
mberezikov2014-stack <mberezikov2014@gmail.com>
2026-09-24 20:43:08 +0500

    переделал olx

--- script13.py
+++ script13.py
@@ -30,20 +30,21 @@ from math import hypot
 import requests
 from bs4 import BeautifulSoup
 
-Category = 'Хостел'
+Category = 'Хостелы'
 Language = 'uzb/'
-Url = 'https://www.olx.uz/'
+Url = 'https://www.olx.uz/d/obyavlenie/kvartira-mehmonxona-xostel-hotel-kunlik-ijara-ID3XQRj.html?reason=hp%7Cpromoted'
 Host = Url + Category + Language
 print(Host)
 Header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/153.0.0.0 Safari/537.36 Edg/153.0.0.0'}
 
-html = requests.get(Host, headers = Header).text
+html = requests.get(Host, headers = Header)
+
 
 soup = BeautifulSoup(html, 'html.parser')
 articles = soup.find_all('div', class_ = 'article')
 DATA = []
 
 for article in articles:
-    title = article_find('div' , class_ ='nt').find('h3').get_text(strip = True)
-    article_link = Url + article_find('div' , class_='nt').find('a').find('h3').get['href']
+    title = article_find('div' , class_ = 'nt').find('h3').get_text(strip = True)
+    article_link = Url + article_find('div' , class_= 'nt').find('a').find('h3').get['href']
     print(article_link)
